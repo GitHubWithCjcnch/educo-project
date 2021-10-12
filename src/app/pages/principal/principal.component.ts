a0b8f67dc9d96c5dd1eb31693d280eefc67072e8
Fabiano Barros <fabianorocha102003@gmail.com>
2021-10-12 18:54:37 -0300

    ngif pra consertar o erro da página carregar primeiro que a requisição

--- src/app/pages/principal/principal.component.ts
+++ src/app/pages/principal/principal.component.ts
@@ -1,25 +1,22 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { StatusService } from '../../services/status.service'
+import { Component, OnInit } from '@angular/core';
+import { StatusService } from '../../services/status.service';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.scss']
 })
-export class PrincipalComponent implements OnInit, AfterViewInit {
+export class PrincipalComponent implements OnInit {
   constructor(
-    public service: StatusService,
+    public service: StatusService
     ) {
-
    }
 
+
+
   ngOnInit(): void {
-    //console.log(this.service.token)
-    //console.log(this.service.userId)
-  }
-  ngAfterViewInit(): void{
     this.service.getUserById(this.service.userId)
-    console.log(this.service.user)
+
   }
 
 
